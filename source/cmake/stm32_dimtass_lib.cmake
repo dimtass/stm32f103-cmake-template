bc642735f9900da7f33a235f0b8a91f86bf5274a
Dimitris Tassopoulos <dimitris.tassopoulos@native-instruments.de>
2019-06-03 14:26:53 +0200

    Fixed the cmake build for my own libs

--- source/cmake/stm32_dimtass_lib.cmake
+++ source/cmake/stm32_dimtass_lib.cmake
@@ -20,14 +20,3 @@ set(STM32_DIMTASS_LIB_SRC
     ${STM32_DIMTASS_LIB_DIR}/src/overclock_stm32f103.c
     ${STM32_DIMTASS_LIB_DIR}/src/tiny_printf.c
 )
-
-set_source_files_properties(${STM32_DIMTASS_LIB_SRC}
-    PROPERTIES COMPILE_FLAGS ${STM32_DEFINES}
-    # STM32_DEFINES are defined in the top level CMakeLists.txt
-)
-
-add_library(stm32dimtasslib STATIC ${STM32_DIMTASS_LIB_SRC})
-
-set_target_properties(stm32dimtasslib PROPERTIES LINKER_LANGUAGE C)
-
-set(EXTERNAL_LIBS ${EXTERNAL_LIBS} stm32dimtasslib)
